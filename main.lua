7d528d7e7de9d24b6877b7e8f831efd3b1a534d9
OwenLaRosa <owen.larosa@outlook.com>
2019-08-10 16:12:17 -0700

    feat: Change title

--- main.lua
+++ main.lua
@@ -79,7 +79,7 @@ function love.load()
     math.randomseed(os.time())
 
     -- app window title
-    love.window.setTitle('Fifty Bird')
+    love.window.setTitle('Fighter Bird')
 
     -- initialize our nice-looking retro text fonts
     smallFont = love.graphics.newFont('font.ttf', 8)
